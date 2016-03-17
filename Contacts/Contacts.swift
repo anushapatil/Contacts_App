5f5605143d100f041ac97f2b71b937b64ff11fb2
Anusha Patil <Anusha@Danger.local>
2016-03-17 22:31:39 +0530

    File add

---
+++ Contacts/Contacts.swift
@@ -0,0 +1,16 @@
+//
+//  Contacts.swift
+//  Contacts
+//
+//  Created by Anusha Patil on 12/03/2016.
+//  Copyright © 2016 Anusha Patil. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Contacts: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
